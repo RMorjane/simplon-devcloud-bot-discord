6e84cc0475d8e5862093a170567da3dd03e8e803
Marwa <majbrim@gmail.com>
2021-03-17 10:35:44 +0100

    added message repply func

--- index.ts
+++ index.ts
@@ -4,9 +4,20 @@ const client = new Discord.Client();
 const env = require('dotenv').config();
 
 client.once('ready', () => {
+    let generalChannel = client.channels.cache.find(channel => channel.name === "commandes-bot4")
+    generalChannel.send("Damin is watching") 
     console.log('ready !')
 })
 
+let my_greeting = ["hello", "Hello", "Hola", "hola", "Bonjour", "bonjour", "Salut", "salut", "Yo", "yo"]
+
+client.on("message", (receivedMessage) => {
+    //https://stackoverflow.com/questions/237104/how-do-i-check-if-an-array-includes-a-value-in-javascript
+    if (my_greeting.includes(receivedMessage.content)) {
+        receivedMessage.channel.send(receivedMessage.content + " " + receivedMessage.author.toString() + " !");
+        //toString anable message to convert the id to the username
+    }
+});
 
 
 client.login(process.env.token);
