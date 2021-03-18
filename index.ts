7db5b0b8644c27a9a4ef91a950b5153047c08e0f
Tiphnm <76214478+Tiphnm@users.noreply.github.com>
2021-03-18 13:55:51 +0100

    Adding logging info on ready function to put the bot online in index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const log = require('simple-node-logger').createSimpleFileLogger('loggings.log'); 
  
 const env = require('dotenv').config();
 
 client.once('ready', () => {
+    log.info("READY start: the Damin bot is online")
     console.log('ready !')
-})
+    log.info("READY end: the Damin bot is online")
 
+})
 
 
 client.login(process.env.token);
