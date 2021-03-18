8f739bc9e52e498bef86c0f135aeaa92b427d0a1
Marwa <majbrim@gmail.com>
2021-03-18 12:57:59 +0100

    putting y function to another field to test it

--- index.ts
+++ index.ts
@@ -1,24 +1,32 @@
+// declare var process : any;
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
 const env = require('dotenv').config();
+//import replytToGreeting from "./send-message-greeting";
+const replytToGreeting = require("./send-message-greeting.ts");
+//import {{replytToGreeting}} from "./send-message-greeting";
 
-client.once('ready', () => {
-    let generalChannel = client.channels.cache.find(channel => channel.name === "commandes-bot4")
-    generalChannel.send("Damin is watching") 
-    console.log('ready !')
-})
 
-let my_greeting = ["hello", "Hello", "Hola", "hola", "Bonjour", "bonjour", "Salut", "salut", "Yo", "yo"]
+client.once('ready', function () {
+    const generalChannel = client.channels.cache.find(function (channel) { return channel.name === "commandes-bot4"; });
+    //generalChannel.send("Damin is watching");
+    console.log('ready !');
+});
 
-client.on("message", (receivedMessage) => {
+
+const my_greeting = ["hello", "Hello", "Hola", "hola", "Bonjour", "bonjour", "Salut", "salut", "Yo", "yo"];
+
+
+client.on("message", function (receivedMessage) {
     //https://stackoverflow.com/questions/237104/how-do-i-check-if-an-array-includes-a-value-in-javascript
-    if (my_greeting.includes(receivedMessage.content)) {
-        receivedMessage.channel.send(receivedMessage.content + " " + receivedMessage.author.toString() + " !");
+    
+    replytToGreeting(my_greeting,receivedMessage.content, receivedMessage.channel, receivedMessage.author)
+    console.log(typeof(replytToGreeting))
         //toString anable message to convert the id to the username
     }
-});
+);
 
 
 client.login(process.env.token);
 //console.log(process.env.token)
+module.exports = client;
